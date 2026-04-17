9218606760d38616243b103ed438e680cba2bc0f
Deeksha Singh Gaur <124994085+deekshasinghgaur97@users.noreply.github.com>
2026-04-17 17:14:57 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,32 @@
+const accountId = 1001;
+
+let accountEamil = "deekshagaur.com"
+accountEamil = "gaurdeeksha.com"
+
+var accountPassword = "12345"
+
+accountCity = "Kanpur"
+
+console.log(accountEamil);
+
+
+let accountState;
+
+ //accountId = 2        //not allowed
+ 
+console.log( accountState);
+ 
+accountEamil = "gaurDeeksha.com"
+accountPassword = "19945"
+accountCity = "jaipur"
+
+/* 
+Prefer not to use var bcz
+of issue in block and functional scope
+*/
+
+// console.log(accountEamil);
+// console.log(accountPassword);
+
+
+// console.table([accountId, accountEamil, accountPassword, accountCity, accountState])
---
+++ 02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all JS code as newer version 
+
+
+
+// window.alert("hello"); //we are useing node JS, not browser
+// alert("hello");
+
+// console.log( v 
+//     +
+//     3)  //code readbility should be high
+    // tc39 
+
+let name = null;
+
+
+let age = 18
+console.log(typeof name);
+let isLoggedin = false
+
+//console.table([name,age,isLoggedin])
+
+//number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true/false
+//null => standalone value / empty value
+// undefined => value not assign
+// symbol => unique/figma tools
+//object
+
+// console.log(typeof undefined);  // undefined
+// console.log(typeof null); // null
+
